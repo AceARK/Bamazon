9913ec615d15d6fe03c60234c746419e06c4c225
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-03-03 00:47:00 -0500

    Supervisor level complete.

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -50,6 +50,14 @@ function getSupervisorAction() {
 				viewProductSalesByDepartment();
 				break;
 
+			case "Create New Department":
+				createNewDepartment();
+				break;
+
+			case "Exit":
+				process.exit();
+				break;
+
 			default:
 				// Random message (helps to check if switch cases are being met)
 				console.log("Bamazon... where we make up in service, what's lacking in name.");
@@ -60,13 +68,56 @@ function getSupervisorAction() {
 
 // Function to view product sales by department
 function viewProductSalesByDepartment() {
+	// Getting data from departments table and displaying using Alias
 	connection.query("SELECT departments.department_id, departments.department_name, departments.over_head_costs, departments.total_sales AS product_sales, (departments.total_sales - departments.over_head_costs) AS total_profit FROM departments", function(err, data) {
 		if(err){
 			console.log(err);
 		}else {
+			// Create some space for better looking output
+			console.log(`
+
+`);			// Logging data in tabular format
 			console.table(data);
+			console.log(`
+`);
 		}
-		process.exit();
+		// Show options again
+		getSupervisorAction();
 	});
 }
 
+
+function createNewDepartment() {
+	inquirer.prompt([
+		{
+			name: "name",
+			message: "Enter the name of the department to be added:" 
+		},
+		{
+			name: "overHeadCosts",
+			message: "Enter the overhead cost for this department:"
+		},
+		{
+			name: "totalSales",
+			message: "Enter total sales from this department(If 0, enter 0 or leave blank):"
+		}
+	]).then(function(department) {
+		if(department.totalSales === (null || "")) {
+			var total_sales = 0.00;
+		}else {
+			var total_sales = department.totalSales;
+		}
+		connection.query("INSERT INTO departments SET ?", {
+			department_name: department.name,
+			over_head_costs: department.overHeadCosts,
+			total_sales: total_sales
+			}, function(err, data) {
+			if(err) {
+				console.log(err);
+			}else {
+				console.log("Department added");
+			}
+			getSupervisorAction();
+		});
+	})
+}
